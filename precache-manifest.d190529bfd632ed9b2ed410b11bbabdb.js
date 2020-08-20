67131a891e8a74d4ae1562bfb6e629c49dc45bf1
CFCode-git <435184766@qq.com>
2020-08-20 11:06:40 +0800

    deploy

---
+++ precache-manifest.d190529bfd632ed9b2ed410b11bbabdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7ea619fdb1c69c0df04aa357ad1bde",
+    "url": "/moneygrubber-React-website1.0/index.html"
+  },
+  {
+    "revision": "4d88f4e85bbb3a976647",
+    "url": "/moneygrubber-React-website1.0/static/css/main.8eef2a44.chunk.css"
+  },
+  {
+    "revision": "0c3beffd7e9d8855086c",
+    "url": "/moneygrubber-React-website1.0/static/js/2.b51494c8.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/moneygrubber-React-website1.0/static/js/2.b51494c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d88f4e85bbb3a976647",
+    "url": "/moneygrubber-React-website1.0/static/js/main.4e4a4123.chunk.js"
+  },
+  {
+    "revision": "4758dba821b7085eb7b5",
+    "url": "/moneygrubber-React-website1.0/static/js/runtime-main.8df26a0d.js"
+  }
+]);
